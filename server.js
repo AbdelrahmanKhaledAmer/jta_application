0d61dff9df5ebe7fcfd668a6a548d512cbf430ea
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2019-02-18 00:54:48 +0200

    feat: mongodb server running

--- server.js
+++ server.js
@@ -2,12 +2,11 @@
 // Requiring Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Initialize express app
 const app = express();
 
-// Give your server a port to listen to
-const port = 8080;
 
 // Use BodyParser as Middleware
 // Middleware is a piece of code that your program runs in the middle of running other code.
@@ -17,6 +16,21 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.json("Hello, World!");
 });
+
+// Connect to mongodb
+const DBName = "jta_app"
+mongoose.connect("mongodb://127.0.0.1/" + DBName)
+    // .then happens when successfully connected
+    .then(() => {
+        console.log("Successfully connectd to database " + DBName);
+    })
+    // .catch happens when connection fails
+    .catch((err) => {
+        console.log(err);
+    });
+
+// Give your server a port to listen to
+const port = 8080;
 
 // App starts listening on given port   
 app.listen(port, () => {
