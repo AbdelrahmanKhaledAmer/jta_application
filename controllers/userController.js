6c1bc2e8e9d30068557967075f823811f0a16631
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2019-02-20 21:13:52 +0200

    feat: user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,13 @@
+// Requiring JWT
+const jwt = require("jsonwebtoken");
+
+// Requiring the user model
 const User = require("../models/user.js")
 
 const userController = {
     signUp: (req, res) => {
         // User did not provide relevant information
-        if (!req.body.email | !req.body.name | !req.body.password) {
+        if (!req.body.email || !req.body.name || !req.body.password) {
             // Bad request
             res.status(400);
             res.json({
@@ -43,6 +47,53 @@ const userController = {
                 "message": "Your account has been created successfully, try logging in."
             });
         })
+    },
+    login: (req, res) => {
+        if (!req.body.email || !req.body.password) {
+            // Bad request
+            res.status(400);
+            res.json({
+                "message": "Not enough information has been provided. Pleas make sure you entered all the correct info."
+            });
+            return;
+        }
+        User.findOne({ email: req.body.email }, (err, userData) => {
+            // Database error occured. Return error status code and message.
+            if (err) {
+                // Internal server error
+                res.status(500);
+                res.json({
+                    "message": "There has been an error while connecting with our database. Please try again later."
+                });
+                return;
+            }
+            // No user with this email exists.
+            if (!userData) {
+                // Forbidden
+                res.status(403);
+                res.json({
+                    "message": "No user with this email exists. Try signing up."
+                });
+                return;
+            }
+            // Wrong password enetered.
+            if (userData.password != req.body.password) {
+                res.json(403);
+                res.json({
+                    "message": "There was an error in your information. Please try again."
+                });
+                return;
+            }
+            // Create a token containing the user's data
+            let token = jwt.sign({ "email": userData.email }, "secretKey", {
+                expiresIn: "30d" // Expires after a month
+            });
+            res.status(200);
+            res.json({
+                "message": "Logged in successfully.",
+                "token": token
+            });
+        });
     }
 }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const controller = require("../controllers/userController.js");
 
 router.post('/api/signup', controller.signUp);
-router.post('/api/login');
+router.post('/api/login', controller.login);
 
 module.exports = router;
